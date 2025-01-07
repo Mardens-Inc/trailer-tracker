b14644531e065e8d23c6385528ffd4e15164c165
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-07 10:46:53 -0500

    Add FleetItemComponent skeleton to sidebar
    Introduce a new FleetItemComponent for the fleet overview sidebar. Currently includes a basic structure with props but no implemented functionality or UI elements yet.

---
+++ src/assets/components/fleet-overview/sidebar/FleetItemComponent.tsx
@@ -0,0 +1,15 @@
+import FleetItem from "../../../ts/fleet-item.ts";
+
+interface FleetItemComponentProps
+{
+    fleetItem: FleetItem;
+    onFleetItemClick: (fleetItem: FleetItem) => void;
+}
+
+export default function FleetItemComponent(props: FleetItemComponentProps)
+{
+    return (
+        <>
+        </>
+    );
+}
