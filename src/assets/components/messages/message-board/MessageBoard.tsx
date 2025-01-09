f7935ffce43d15e5952e3d5c1b1d55c9341dab9a
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-09 15:14:50 -0500

    Add MessageBoard component with basic structure
    Introduce a new MessageBoard component with a minimal setup and props. Currently, it takes a `fleet` prop but has no rendering logic or content yet. This sets the foundation for further development.

---
+++ src/assets/components/messages/message-board/MessageBoard.tsx
@@ -0,0 +1,14 @@
+import FleetItem from "../../../ts/fleet-item.ts";
+
+interface MessageBoardProps
+{
+    fleet: FleetItem
+}
+
+export default function MessageBoard(props: MessageBoardProps)
+{
+    return (
+        <div>
+        </div>
+    );
+}
