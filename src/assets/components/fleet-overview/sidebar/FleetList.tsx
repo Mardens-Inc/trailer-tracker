0a7eb8039224daabb73fae227b29ea8deafaa1b2
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-09 15:16:31 -0500

    Add pagination and item component to FleetList.tsx
    Introduced pagination using NextUI's Pagination component to manage fleet item display. Replaced inline item rendering with a dedicated FleetItemComponent for better modularity. Adjusted layout and scrolling behavior to enhance user experience.

--- src/assets/components/fleet-overview/sidebar/FleetList.tsx
+++ src/assets/components/fleet-overview/sidebar/FleetList.tsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
 import FleetItem from "../../../ts/fleet-item.ts";
 import FleetManagement from "../../../ts/fleet-management.ts";
-import {Spinner} from "@nextui-org/react";
+import {Pagination, Spinner} from "@nextui-org/react";
+import FleetItemComponent from "./FleetItemComponent.tsx";
 
 export default function FleetList()
 {
@@ -20,19 +21,45 @@ export default function FleetList()
         {
             console.log(fleet);
             setPageItems(fleet.slice(0, itemsPerPage));
-            setTotalPages(Math.ceil(fleet.length / itemsPerPage));
+            setTotalPages(Math.floor(fleet.length / itemsPerPage));
             setItems(fleet);
         }).catch(error =>
         {
             console.log(error);
         }).finally(() =>
         {
-            // setLoading(false);
+            setLoading(false);
         });
     }, []);
+
+    useEffect(() =>
+    {
+        setPageItems(items.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+    }, [page]);
+
     return (
-        <div className={"w-full"}>
+        <div className={"w-full h-full overflow-y-hidden flex flex-col gap-2"}>
             {loading ? <div className={"flex justify-center"}><Spinner label={"Loading Fleet..."} size={"lg"} color={"primary"}/></div> : null}
+            <div className={"w-full h-full overflow-y-auto flex flex-col gap-2 overflow-x-hidden"}>
+                {pageItems.map(item => (
+                    <FleetItemComponent
+                        key={item.id}
+                        fleetItem={item}
+                        onFleetItemClick={_ =>
+                        {
+                        }}
+                    />
+                ))}
+            </div>
+            <Pagination
+                total={totalPages}
+                initialPage={1}
+                page={page}
+                onChange={setPage}
+                showControls
+                showShadow
+                className={"mx-auto mt-4"}
+            />
         </div>
     );
 }
